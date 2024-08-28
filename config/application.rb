595f6f3105a11b9b62a292d1858416be4e92d265
Myroslav Nastenko <myroslav.nastenko@innovatrivs.com>
2024-08-28 15:57:29 +0200

    Rename project to OrderFlow

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require 'action_cable/engine'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module JuliaStore2
+module OrderFlow
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
