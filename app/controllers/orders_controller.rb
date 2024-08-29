b972cf90512a511fa3836e2a659a7d2b0e02005b
Myroslav Nastenko <myroslav.nastenko@innovatrivs.com>
2024-08-29 16:26:53 +0200

    Edit order with items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,13 +36,9 @@ class OrdersController < ApplicationController
   def update
     result = Orders::UpdateOrderService.new(@order, order_params).call
 
-    render json: result, status: :ok
-
-    # if @order.update(order_params)
-    #   respond_to_success('Order was successfully updated.', :ok)
-    # else
-    #   respond_to_failure(@order.errors, :unprocessable_entity)
-    # end
+    render :show, status: :ok, locals: { order: result }
+  rescue ActiveRecord::RecordInvalid => e
+    render json: e.record.errors, status: :unprocessable_entity
   end
 
   # DELETE /orders/1 or /orders/1.json
@@ -67,19 +63,5 @@ class OrdersController < ApplicationController
       :street, :post_code, :building, :flat, :note, :phone,
       order_items_attributes: %i[id item_id count price]
     )
-  end
-
-  def respond_to_success(notice, status)
-    respond_to do |format|
-      format.html { redirect_to order_url(@order), notice: }
-      format.json { render :show, status:, location: @order }
-    end
-  end
-
-  def respond_to_failure(errors, status)
-    respond_to do |format|
-      format.html { render :edit, status: }
-      format.json { render json: errors, status: }
-    end
   end
 end
--- app/services/orders/update_order_service.rb
+++ app/services/orders/update_order_service.rb
@@ -12,16 +12,53 @@ module Orders
     end
 
     def call
-      existing_items = order.order_items.pluck(:item_id)
-      new_items = params[:order_items_attributes].map { |order_item| order_item[:item_id] }
+      Order.transaction do
+        update_order_attributes
+        update_order_items
+        order.save!
+      end
 
-      compared_items = compare_arrays(existing_items, new_items)
+      order
+    end
 
-      puts "=================================="
-      puts "items to add: #{compared_items[:new]}"
-      puts "items to delete: #{compared_items[:deleted]}"
-      puts "items to update: #{compared_items[:existed]}"
-      puts "=================================="
+    private
+
+    def update_order_attributes
+      order.assign_attributes(order_params)
+    end
+
+    def update_order_items
+      existing_items = order.order_items.index_by(&:item_id)
+      new_items_params = params[:order_items_attributes].index_by { |item| item[:item_id] }
+
+      items_to_delete = existing_items.keys - new_items_params.keys
+      items_to_create = new_items_params.keys - existing_items.keys
+      items_to_update = existing_items.keys & new_items_params.keys
+
+      destroy_order_items(items_to_delete)
+      update_existing_items(existing_items, new_items_params, items_to_update)
+      create_new_items(new_items_params, items_to_create)
+    end
+
+    def destroy_order_items(ids)
+      order.order_items.where(item_id: ids).destroy_all
+    end
+
+    def update_existing_items(existing_items, new_items_params, ids)
+      ids.each do |item_id|
+        order_item = existing_items[item_id]
+        order_item.update!(new_items_params[item_id].except(:id))
+      end
+    end
+
+    def create_new_items(new_items_params, ids)
+      ids.each do |item_id|
+        order.order_items.create!(new_items_params[item_id])
+      end
+    end
+
+    def order_params
+      params.except(:order_items_attributes, :id)
     end
   end
 end
